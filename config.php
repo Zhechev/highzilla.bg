c366adc5f10366dbf14ed224e89726031e792c62
Zhechev <mitko_jechev@mail.bg>
2018-08-22 12:34:36 +0300

    init commit clean opencart -v 2.3.0.2

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://dev.highzilla.bg/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://dev.highzilla.bg/');
+
+// DIR
+define('DIR_APPLICATION', '/home/braincha/public_html/dev.highzilla.bg/catalog/');
+define('DIR_SYSTEM', '/home/braincha/public_html/dev.highzilla.bg/system/');
+define('DIR_IMAGE', '/home/braincha/public_html/dev.highzilla.bg/image/');
+define('DIR_LANGUAGE', '/home/braincha/public_html/dev.highzilla.bg/catalog/language/');
+define('DIR_TEMPLATE', '/home/braincha/public_html/dev.highzilla.bg/catalog/view/theme/');
+define('DIR_CONFIG', '/home/braincha/public_html/dev.highzilla.bg/system/config/');
+define('DIR_CACHE', '/home/braincha/public_html/dev.highzilla.bg/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/braincha/public_html/dev.highzilla.bg/system/storage/download/');
+define('DIR_LOGS', '/home/braincha/public_html/dev.highzilla.bg/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/braincha/public_html/dev.highzilla.bg/system/storage/modification/');
+define('DIR_UPLOAD', '/home/braincha/public_html/dev.highzilla.bg/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'braincha_highzilla');
+define('DB_PASSWORD', 'J!=v4.,}%1rn');
+define('DB_DATABASE', 'braincha_dev.highzilla.bg');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
