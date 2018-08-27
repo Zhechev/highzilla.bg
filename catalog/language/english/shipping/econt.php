ae247a6c0f533348c2be4fe66748bd74e67839a8
Zhechev <zhechev.mitko@gmail.com>
2018-08-27 23:42:17 +0300

    econt module was added

---
+++ catalog/language/english/shipping/econt.php
@@ -0,0 +1,62 @@
+<?php
+// Text
+$_['text_title']                         = 'Econt Express';
+$_['text_description']                   = 'Econt Express';
+$_['text_calculate']                     = 'Please Click "Calculate Price" after filling/modifying your data.';
+$_['text_wait']                          = ' --- Please, wait --- ';
+$_['text_to_office']                     = 'to office';
+$_['text_to_door']                       = 'to door';
+$_['text_to_aps']                        = 'to APS';
+$_['text_value']                         = 'value';
+$_['text_before']                        = 'before';
+$_['text_in']                            = 'in';
+$_['text_after']                         = 'after';
+$_['text_hour']                          = 'h.';
+$_['text_day_0']                         = 'Sunday';
+$_['text_day_1']                         = 'Monday';
+$_['text_day_2']                         = 'Tuesday';
+$_['text_day_3']                         = 'Wednesday';
+$_['text_day_4']                         = 'Thursday';
+$_['text_day_5']                         = 'Friday';
+$_['text_day_6']                         = 'Saturday';
+$_['text_e1']                            = 'to 60 min (Е1)';
+$_['text_e2']                            = 'to 90 min (Е2)';
+$_['text_e3']                            = 'to 120 min (Е3)';
+$_['text_dc']                            = 'Acknowledgment of receipt service added.';
+$_['text_dc_cp']                         = 'Acknowledgment of receipt/Stock receipt service added.';
+$_['text_invoice_before_cd']             = 'Deliver invoice before payment of cash on delivery service added.';
+$_['text_pay_after_accept']              = 'The trader allows the stock to be reviewed by the receiver and to be paid only if the stock is accepted.';
+$_['text_pay_after_test']                = 'The trader allows the stock to be reviewed and be tested by the receiver and to be paid only if the stock is accepted.';
+$_['text_partial_delivery']              = 'Possibility for partial delivery.';
+$_['text_services_aps']                  = 'При доставка до "24 часа Еконт - Автоматична пощенска станция”(АПС) единствената възможна услуга е наложен платеж, а заплащането на сумата и разходите за доставка става на място с карта.';
+$_['text_processing']                    = 'The order is being processed';
+$_['text_modify']                        = 'Modify &raquo;';
+$_['text_aps_info']                      = 'С “24 часа Еконт - Автоматична пощенска станция”(АПС) е устройство, с което сами изпращате и получавате пратки денонощно, без почивен ден.<br/>Научете повече на: <a target="_blank" href="http://www.econt.com/24-chasa-econt-aps/">http://www.econt.com/24-chasa-econt-aps/</a>';
+
+// Entry
+$_['entry_shipping_to']                  = 'Delivery:';
+$_['entry_post_code']                    = 'Postal code:';
+$_['entry_city']                         = 'Settlement:';
+$_['entry_quarter']                      = 'Neighbourhood:';
+$_['entry_street']                       = 'Street:';
+$_['entry_street_num']                   = 'No.:';
+$_['entry_other']                        = 'Other:';
+$_['entry_office']                       = 'Office:';
+$_['entry_office_code']                  = 'Office code:';
+$_['entry_cd']                           = 'Cash on delivery:';
+$_['entry_priority_time']                = 'Priority hour:';
+$_['entry_express_city_courier']         = 'Express city courier:';
+$_['entry_delivery_day']                 = 'Delivery day:';
+
+// Button
+$_['button_office_locator']              = 'Office Locator';
+
+// Error
+$_['error_connect']                      = 'An error occurred while connecting with Econt! Please try again!';
+$_['error_calculate']                    = 'Please, fill correctly all fields!';
+$_['error_address']                      = 'Invalid address!';
+$_['error_office']                       = 'Please, select office!';
+$_['error_office_not_found']             = 'No office found!';
+$_['error_priority_time']                = 'Please, select hour!';
+$_['error_cd_payment_aps']               = 'Upon delivery to the APS service is only possible cash and need pay the amount and cost of delivery Card!';
+?>
